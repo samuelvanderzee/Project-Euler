796abe2b634875e0e33b0a16fceca66ddae5c4b6
samuelvanderzee <49663282+samuelvanderzee@users.noreply.github.com>
2019-04-15 20:30:55 -0500

    Add files via upload

---
+++ euler97.m
@@ -0,0 +1,13 @@
+a = 2^24
+product = 1
+c = 28433
+for i = 24:24:7830456
+ product = product*a
+ if product > 10^10
+   product = mod(product, 10^10)
+   answer = mod(2*c*product + 1, 10^10)
+   
+ endif
+endfor
+
+
