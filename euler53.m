9162914917188795576fd465ab12ee177067abc9
samuelvanderzee <49663282+samuelvanderzee@users.noreply.github.com>
2019-04-15 20:22:49 -0500

    Add files via upload

---
+++ euler53.m
@@ -0,0 +1,14 @@
+number = 0;
+for n = 1:100
+  for r = 1:100
+    if (n-r)>= 0 && r <= n
+    m = (factorial(n)/(factorial(r)*factorial(n-r)));
+    if m>1000000
+      number++;
+      
+    endif
+    endif
+  endfor
+endfor
+
+disp(number)
