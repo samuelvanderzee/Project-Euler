23c69cfff038eb65fbd6e0e54f37866d5e86c4e4
samuelvanderzee <49663282+samuelvanderzee@users.noreply.github.com>
2019-04-15 19:48:56 -0500

    Add files via upload

---
+++ euler58.m
@@ -0,0 +1,25 @@
+numdiagonals = 1;
+numprimes = 0;
+
+for i = 3:2:50000
+ numdiagonals = numdiagonals + 4;
+ if isprime(i*i)==true
+   numprimes++;   
+ endif
+ if isprime((i*i) - (i-1)) == true
+   numprimes++;   
+ endif
+ 
+ if isprime((i*i) - 2*(i-1)) == true
+   numprimes++;
+ endif
+ if isprime((i*i) - 3*(i-1)) == true
+   numprimes++;
+   
+   endif
+if numprimes/numdiagonals < 0.1
+  sidelength = i
+  break;
+endif
+disp(i)
+ endfor
