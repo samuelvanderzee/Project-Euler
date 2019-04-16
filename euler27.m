e5b7896d610b3b3ea511e5076c2254c7e28e1883
samuelvanderzee <49663282+samuelvanderzee@users.noreply.github.com>
2019-04-15 19:42:09 -0500

    Add files via upload

---
+++ euler27.m
@@ -0,0 +1,20 @@
+maxcount = 0
+for a = -999:999
+  for b = 2:999
+    n = 0;
+    count = 0;
+    disp(a)
+    disp(b)
+      while isprime(n*n + a*n + b) == true
+        n++;
+        count++;     
+       
+        if count > maxcount
+          maxcount = count;
+          productofcoefficients = a*b
+          disp(maxcount);      
+        end
+    end
+  end
+end
+disp(productofcoefficients)
