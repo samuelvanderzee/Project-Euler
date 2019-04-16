395d915862d81cd848ba3b1dc482abac915eb266
samuelvanderzee <49663282+samuelvanderzee@users.noreply.github.com>
2019-04-15 19:47:08 -0500

    Add files via upload

---
+++ euler85.m
@@ -0,0 +1,13 @@
+mindistance = 2000000;
+for i = 1:200
+  for j = 1:i
+    area = i*j;
+    rectangles = ((i+1)*i*(j+1)*j)/4;
+    distance = abs(rectangles - 2000000);
+    if distance < mindistance
+  mindistance = distance;
+  areaofnearest = area;
+  endif
+  endfor
+endfor
+disp(areaofnearest)
