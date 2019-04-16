dca8636c868b8488f8f47f500ecde11d84eb65f6
samuelvanderzee <49663282+samuelvanderzee@users.noreply.github.com>
2019-04-15 20:25:00 -0500

    Add files via upload

---
+++ euler36.m
@@ -0,0 +1,13 @@
+sum = 0;
+for i = 1:999999
+  a = num2str(i);
+  b = fliplr(a);
+  c = dec2bin(i);
+  d = num2str(c);
+  e = fliplr(d);
+  disp(i)
+  disp(sum)
+  if (a==b) && (d==e)
+    sum = sum+i;
+  endif
+endfor
