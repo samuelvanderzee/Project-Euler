aede39e47e22bc135dd98d7d087fd22818c36922
samuelvanderzee <49663282+samuelvanderzee@users.noreply.github.com>
2019-04-24 14:34:36 -0500

    Add files via upload

---
+++ euler32.m
@@ -0,0 +1,52 @@
+X = [""];
+n1= num2str(1);
+n2= num2str(2);
+n3= num2str(3);
+n4= num2str(4);
+n5= num2str(5);
+n6= num2str(6);
+n7= num2str(7);
+n8= num2str(8);
+n9= num2str(9);
+X = strcat(X, n1, n2, n3, n4, n5, n6, n7, n8, n9)
+productarray = [""]
+for a = 10:99
+  for b = 100:999       
+    str = [""]
+
+    product = a*b
+    concatenation = strcat(str, num2str(product), num2str(a), num2str(b))
+    sortedconcatenation = sort(concatenation)
+    if (tf = isequal(X, sortedconcatenation) == true)
+      productstring = num2str(product)
+      productarray = strcat(productarray, productstring)
+    endif
+      
+    
+  endfor
+endfor
+
+for a = 2:9
+  for b = 1000:4999
+     str = [""]
+product = a*b
+concatenation = strcat(str, num2str(product), num2str(a), num2str(b))
+sortedconcatenation = sort(concatenation)
+if (tf = isequal(X, sortedconcatenation) == true)
+      productstring = num2str(product)
+      productarray = strcat(productarray, " ", productstring)
+      endif
+ 
+  endfor
+endfor
+k = 1
+for i = 1:length(productarray)
+  if mod(i, 4) == 1
+    
+    productofnumbers(k) = str2num(productarray(i:i+3))  
+    k++;
+  endif
+endfor
+A = unique(productofnumbers)
+clear sum
+B = sum(A)
