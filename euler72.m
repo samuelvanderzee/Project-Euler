4be4ca97b23508aa41f01f8f330a68957e25d652
samuelvanderzee <49663282+samuelvanderzee@users.noreply.github.com>
2019-05-08 11:24:34 -0500

    Add files via upload

---
+++ euler72.m
@@ -0,0 +1,11 @@
+count = 0;
+for d = 1:1000000
+a = unique(factor(d));
+product = d;
+for i = 1:length(a)
+  product = product*(1 - 1/(a(i)));
+endfor
+phi = product;
+count = count+phi;
+endfor
+disp(count)
